776a5ade19fc7eff04b7dbc0a5ca1919b3c288cc
Cristina Mateo <cris.mateo.99@gmail.com>
2023-10-05 10:32:15 +0200

    corregido

--- script.js
+++ script.js
@@ -59,9 +59,8 @@ let header = document.createElement("header")
 let main =document.createElement("main")
 let footer =document.createElement("footer")
 let nombre =document.createElement("h1")
+let t1 = document.createTextNode("Minibiblioteca")
 
-let t1 = document.createTextNode("")
-t1.textContent = "Minibiblioteca"
 
 body.appendChild(header)
 body.appendChild(main)
@@ -135,68 +134,22 @@ for (let i=0;i<books.length;i++){
 let segunda = document.createElement("section")
 main.appendChild(segunda)
 
+let fase2 = ``
 
-const fase2 = 
-  ` <article>
-        <h3>${books[0].title}</h3>
-        <img src= ${books[0].imageLink}>
+for(let i=0; i<books.length; i++){
+ fase2+= ` <article>
+        <h3>${books[i].title}</h3>
+        <img src= ${books[i].imageLink}>
         <ul>
-          <li>Author ${books[0].author}</li>
-          <li>Country ${books[0].country}</li>
-          <li>Language ${books[0].language}</li>
-          <li>Pages ${books[0].pages}</li>
-          <li>Year ${books[0].year}</li>
-          <a href=${books[0].link}>More information</a>
+          <li>Author ${books[i].author}</li>
+          <li>Country ${books[i].country}</li>
+          <li>Language ${books[i].language}</li>
+          <li>Pages ${books[i].pages}</li>
+          <li>Year ${books[i].year}</li>
+          <a href=${books[i].link}>More information</a>
         </ul>
-      </article>
-      <article>
-        <h3>${books[1].title}</h3>
-        <img src= ${books[1].imageLink}>
-        <ul>
-          <li>Author ${books[1].author}</li>
-          <li>Country ${books[1].country}</li>
-          <li>Language ${books[1].language}</li>
-          <li>Pages ${books[1].pages}</li>
-          <li>Year ${books[1].year}</li>
-          <a href=${books[1].link}>More information</a>
-        </ul>
-      </article>
-      <article>
-        <h3>${books[2].title}</h3>
-        <img src= ${books[2].imageLink}>
-        <ul>
-          <li>Author ${books[2].author}</li>
-          <li>Country ${books[2].country}</li>
-          <li>Language ${books[2].language}</li>
-          <li>Pages ${books[2].pages}</li>
-          <li>Year ${books[2].year}</li>
-          <a href=${books[2].link}>More information</a>
-        </ul>
-      </article>
-      <article>
-        <h3>${books[3].title}</h3>
-        <img src= ${books[3].imageLink}>
-        <ul>
-          <li>Author ${books[3].author}</li>
-          <li>Country ${books[3].country}</li>
-          <li>Language ${books[3].language}</li>
-          <li>Pages ${books[3].pages}</li>
-          <li>Year ${books[3].year}</li>
-          <a href=${books[3].link}>More information</a>
-        </ul>
-      </article>
-      <article>
-        <h3>${books[4].title}</h3>
-        <img src= ${books[4].imageLink}>
-        <ul>
-          <li>Author ${books[4].author}</li>
-          <li>Country ${books[4].country}</li>
-          <li>Language ${books[4].language}</li>
-          <li>Pages ${books[4].pages}</li>
-          <li>Year ${books[4].year}</li>
-          <a href=${books[4].link}>More information</a>
-        </ul>
-      </article>`;
+      </article>`
+}
   
     segunda.innerHTML = fase2
 
